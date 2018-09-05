522cf179aec2310d76061fdd0049c59e6b24a6fb
Sedin Dugum <sedin.dugum@gmail.com>
2018-09-05 09:48:22 +0200

    cleanup

--- app/flow/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/sedind/flow/app/flow/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- app/response.go
+++
@@ -1,8 +0,0 @@
-package app
-
-// Response represents response object for API
-type Response struct {
-	Data    interface{} `json:"data"`
-	Error   interface{} `json:"error"`
-	Success bool        `json:"success"`
-}
